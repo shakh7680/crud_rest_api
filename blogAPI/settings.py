152b519c0654341bafa0e72ac9dca6b9a1b19a22
shakh7680 <shahzodikromovich7680@gmail.com>
2022-04-19 13:59:27 +0500

    serializers.py and urls.py added

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -29,15 +29,22 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    'rest_framework',
+]
+
+CUSTOM_CREATED_APPS = [
+    'apps.posts'
+]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+                    'django.contrib.admin',
+                    'django.contrib.auth',
+                    'django.contrib.contenttypes',
+                    'django.contrib.sessions',
+                    'django.contrib.messages',
+                    'django.contrib.staticfiles',
+                ] + THIRD_PARTY_APPS + CUSTOM_CREATED_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
