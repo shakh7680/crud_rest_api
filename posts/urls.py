f80875c097e164713d9cc4671c56a717d565c09e
shakh7680 <shahzodikromovich7680@gmail.com>
2022-04-19 23:04:03 +0500

    Adding viewsets and routers

--- posts/urls.py
+++ posts/urls.py
@@ -4,10 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import SimpleRouter
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('<int:pk>', PostDetailView.as_view()),
     path('', PostListView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Post
-from .serializers import PostSerializer
+from django.contrib.auth import get_user_model
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 class PostListView(generics.ListAPIView):
@@ -11,3 +13,7 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthorOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
